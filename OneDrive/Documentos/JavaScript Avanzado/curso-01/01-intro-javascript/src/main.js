ccc14ec9cc9332d2ce8ab40492b942434f5c95f0
Miwa Martinez <miwa.mtz@gmail.com>
2025-09-25 15:25:44 -0300

    Declarando variables en Javascript

--- OneDrive/Documentos/JavaScript Avanzado/curso-01/01-intro-javascript/src/main.js
+++ OneDrive/Documentos/JavaScript Avanzado/curso-01/01-intro-javascript/src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+console.warn("Mensajes de consola")
+//! Constructores de variables
+console.log("Nivel informativo")
+console.warn("Nivel de precaucion")
+console.error("Nivel de error critico") //se rompio 
+
+//VAR (no usar)
+//LET (ES6)
+
+let palabra = 'Hola' //cadena (string)
+console.log(palabra);
+let nombre = 'Maxi' //cadena (string)
+console.log(nombre);
+let isActivo = true //Bool
+console.log(isActivo);
+let numero = 22 //Numero
+console.log(numero); 
+let numeroDec = 33.22 // Numero // camelCase para variables
+console.log(numeroDec);
