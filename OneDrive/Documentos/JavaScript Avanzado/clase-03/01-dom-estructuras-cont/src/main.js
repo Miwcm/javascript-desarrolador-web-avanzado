db2542aced1fdd7c74e52b3cbc98224bd574a493
Miwa Martinez <miwa.mtz@gmail.com>
2025-09-30 14:53:52 -0300

    Usamos getElementByClassName

--- OneDrive/Documentos/JavaScript Avanzado/clase-03/01-dom-estructuras-cont/src/main.js
+++ OneDrive/Documentos/JavaScript Avanzado/clase-03/01-dom-estructuras-cont/src/main.js
@@ -14,20 +14,23 @@ console.dir(window.document) // sacar radiografía
 console.log(document.children[0].children[1].children[0]) // h1
 
 // ! Métodos clasicos de acceso a los elementos del DOM
-console.warn('// ? getElementById() // Me retorna un HTMLElement');
+console.warn('// ? getElementById() ** // Me retorna un HTMLElement');
 
-const textSecundario = document.getElementById('texto-secundario')
-//console.log(textoSecundario)
+const textoSecundario = document.getElementById('texto-secundario')
+// console.log(textoSecundario)
 console.dir(textoSecundario)
+// https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+// https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+//textoSecundario.style.backgroundColor = 'crimson'
 
 textoSecundario.classList.add('color-fondo-violeta')
 
-if(textSecundario==null){
- //debuger
- console.log('Esta seleccionado correctamente')
-}
-else{
-    //debuger
+if ( textoSecundario ) { // true
+    // debugger
+    console.log('Está seleccionado correctamente');
+} else {
+    // debugger
     console.error('Todo mal...')
 }
 
@@ -38,12 +41,14 @@ console.warn('// ? getElementsByClassName() // Me retorna una colección de HTML
 console.warn('// ? getElementsByTagName() // Me retorna un colección de HTMLElement');
 
 const todosLosTitulosH2 = document.getElementsByTagName('h2')
-console.log(todosLosTitulosH2);// HTMLCollection -> Se va a trabajar como si fuera un array
+console.log(todosLosTitulosH2); // HTMLCollection -> se va a trabajar como si fuera un array
 
-const primerElemento = todosLosTitulosH2[0]
-console.log(primerElemento)
-primerElemento.classList.add('color-fondo-violeta')
+//console.log(todosLosTitulosH2[0])
+//console.dir(todosLosTitulosH2[0])
+const segundoElemento = todosLosTitulosH2[1]
+console.log(segundoElemento)
+segundoElemento.classList.add('color-fondo-violeta')
 
 console.warn('// ? Métodos modernos de acceso a los elementos del DOM');
-console.warn('// ? querySelector() // Me retorna un Nodo');
-console.warn('// ? querySelectorAll() // Me retorno una lista de Nodos');D
+console.warn('// ? querySelector() ** // Me retorna un Nodo');
+console.warn('// ? querySelectorAll() ** // Me retorno una lista de Nodos');
