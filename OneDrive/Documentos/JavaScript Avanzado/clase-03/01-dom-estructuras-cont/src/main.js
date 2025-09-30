b61f7f1da6bb26300e119129972cd7d6ab427f95
Miwa Martinez <miwa.mtz@gmail.com>
2025-09-30 14:51:45 -0300

    Agregar color fondo CSS

--- OneDrive/Documentos/JavaScript Avanzado/clase-03/01-dom-estructuras-cont/src/main.js
+++ OneDrive/Documentos/JavaScript Avanzado/clase-03/01-dom-estructuras-cont/src/main.js
@@ -15,9 +15,35 @@ console.log(document.children[0].children[1].children[0]) // h1
 
 // ! Métodos clasicos de acceso a los elementos del DOM
 console.warn('// ? getElementById() // Me retorna un HTMLElement');
+
+const textSecundario = document.getElementById('texto-secundario')
+//console.log(textoSecundario)
+console.dir(textoSecundario)
+
+textoSecundario.classList.add('color-fondo-violeta')
+
+if(textSecundario==null){
+ //debuger
+ console.log('Esta seleccionado correctamente')
+}
+else{
+    //debuger
+    console.error('Todo mal...')
+}
+
 console.warn('// ? getElementsByClassName() // Me retorna una colección de HTMLElement');
+
+
+
 console.warn('// ? getElementsByTagName() // Me retorna un colección de HTMLElement');
+
+const todosLosTitulosH2 = document.getElementsByTagName('h2')
+console.log(todosLosTitulosH2);// HTMLCollection -> Se va a trabajar como si fuera un array
+
+const primerElemento = todosLosTitulosH2[0]
+console.log(primerElemento)
+primerElemento.classList.add('color-fondo-violeta')
 
 console.warn('// ? Métodos modernos de acceso a los elementos del DOM');
 console.warn('// ? querySelector() // Me retorna un Nodo');
-console.warn('// ? querySelectorAll() // Me retorno una lista de Nodos');
+console.warn('// ? querySelectorAll() // Me retorno una lista de Nodos');D
