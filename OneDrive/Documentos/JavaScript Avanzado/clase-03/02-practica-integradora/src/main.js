a38ca4ea13787b0c76e37610458d0f1d39c0a19c
Miwa Martinez <miwa.mtz@gmail.com>
2025-09-30 16:24:45 -0300

    Empezando con el array

--- OneDrive/Documentos/JavaScript Avanzado/clase-03/02-practica-integradora/src/main.js
+++ OneDrive/Documentos/JavaScript Avanzado/clase-03/02-practica-integradora/src/main.js
@@ -1,24 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const producto1 = {id:1, nombre: 'Notebook', precio: 1200, img: 'https://placehold.co/400'}
+const producto2 = {id:1, nombre: 'PC', precio: 1600, img: 'https://placehold.co/400'}
+const producto3 = {id:1, nombre: 'Auriculares', precio: 1000, img: 'https://placehold.co/400'}
+const producto4 = {id:1, nombre: 'Mouse', precio: 700, img: 'https://placehold.co/400'}
+const producto5 = {id:1, nombre: 'Teclado', precio: 900, img: 'https://placehold.co/400'}
 
-setupCounter(document.querySelector('#counter'))
+const productos = [producto1, producto2, producto3, producto4, producto5]
+
+console.log(productos)
